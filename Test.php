bdb0a740af5003ed2d6a4307c2de8429a93061c0
Juan-Carlos Rodriguez <jrodriguez@jrodriguezmbpr.bbbb.net>
2016-08-15 12:26:56 -0700

    Testing travis new CI

--- Test.php
+++ Test.php
@@ -1,5 +1,9 @@
 <?php
 
+before_install:
+  - sudo apt-get -qq update
+  - sudo apt-get install -y libxml2-dev
+
 class Test extends PHPUnit_Framework_TestCase
 {
 	public function testOnePlusOne() {
